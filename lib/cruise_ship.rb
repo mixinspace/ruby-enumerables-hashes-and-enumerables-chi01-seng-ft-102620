a4d55e29219614d30f1e85a45163159b0f634730
Islam Madkaur <islammadkaur@gmail.com>
2020-10-26 02:16:01 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,13 +8,10 @@
 
 def select_winner(passengers)
   winner = ""
-  passengers.each { |suite, name|
-    if suite && name.start_with("A")
-      winner = name
-
-  }
-  end
-  winner
-
-  # add the code snippet here!
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with("A")
+    winner == name
+end
+end
+winner
 end

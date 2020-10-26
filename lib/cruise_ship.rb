1e59be14be79d176be3fadb8b862eb740c360617
Islam Madkaur <islammadkaur@gmail.com>
2020-10-26 02:20:03 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,12 +8,11 @@
 
 def select_winner(passengers)
   winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
-  end
-end
+  passengers.each { |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
 
+end
+}
 winner
-  # add the code snippet here!
 end
